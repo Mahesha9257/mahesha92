742cce4e51c8bb49b8fc42706a09af159ad3b522
Mahesha9257 <maheshamahi9257@gmail.com>
2023-05-25 14:40:00 +0530

    DATA typ

---
+++ DATA typ.py
@@ -0,0 +1,38 @@
+'''int
+boolean
+ float
+ complex
+ '''
+
+'''
+b=-65
+a=52
+print (type(a),type(b))
+'''
+
+'''float
+b=2.6
+print(type(b))
+'''
+
+
+
+
+
+
+
+
+#a=True
+#b=False
+#print(type(a), type(b))
+
+'''
+print(True==1)
+print(False==0)
+print(True==0)
+print(False==1)
+'''
+
+
+
+
